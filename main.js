6e53d501e950d2cdc34600768099160d328cb71d
Rin <sakimotorin@gmail.com>
2024-05-29 01:42:54 +0800

    commit

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import "./assets/scss/all.scss";
 import * as bootstrap from "bootstrap";
+
+const navbarBtn = document.querySelector('.navbar-toggler');
+navbarBtn.addEventListener('click', e => {
+  e.preventDefault();
+  navbarBtn.classList.toggle('open-menu')
+})
