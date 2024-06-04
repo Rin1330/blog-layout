c30f81c76eb8ace27501c8a41197ffbbaf0af3e9
Rin <sakimotorin@gmail.com>
2024-06-04 08:00:57 +0800

    update home swiper

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 import "./assets/scss/all.scss";
 import * as bootstrap from "bootstrap";
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
 
 const navbarBtn = document.querySelector('.navbar-toggler');
 navbarBtn.addEventListener('click', e => {
   e.preventDefault();
   navbarBtn.classList.toggle('open-menu')
 })
+
+const swiper = new Swiper('#articles .swiper', {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  768: {
+    slidesPerView: 1,
+    spaceBetween: 24
+  },
+  // when window width is >= 640px
+  // 992: {
+  //   slidesPerView: 3,
+  //   spaceBetween: 24,
+  // },
+  pagination: {
+    el: "#articles .swiper-pagination",
+    clickable: true,
+    renderBullet: function(index, className){
+      return `<a href="javascript:;" class="${className} fs-0 w-3 h-3 mx-2 bg-black">${index + 1}</a>`;
+    }
+  }
+});
