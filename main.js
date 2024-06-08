36937fb3ecb5a4c002281f939caa33b400ff1389
Rin <sakimotorin@gmail.com>
2024-06-09 04:44:55 +0800

    modify observer

--- main.js
+++ main.js
@@ -5,54 +5,65 @@ import { gsap } from "gsap";
 
 import "swiper/css/bundle";
 
-const header = document.querySelector("#header");
-const target = document.querySelector("#work");
-const list = ["position-sticky", "top-0", "start-0", "z-5", "bg-white"];
+document.addEventListener('DOMContentLoaded', () => {
 
-function cb(payload) {
-	console.log(payload[0]);
-	console.log(payload[0].isIntersecting);
-	console.log(payload[0].intersectionRatio);
-}
+	const header = document.querySelector('#header');
+	const sections = document.querySelectorAll('#section:not(.main-banner)')
+	const list = ["position-sticky", "top-0", "start-0", "z-5", "bg-white"];
 
-const ob = new IntersectionObserver(cb, {
-	// root: target,
-	// threshold: 0.5,
-});
+	// if(sections.length < 2) return;
 
-ob.observe(target);
+	const option = {
+		root: null,
+		rootMargin: `50px 0px 0px 0px`,
+		threshold: 0.2
+	}
 
-// const handler = entries => {
-//   !entries[0].isIntersection ?  header.classList.add(...list) : header.classList.remove(...list)
-// }
-// const observer = new window.IntersectionObserver(handler, option)
-// observer.observe(header);
+	const callBack = (entries, observe) => {
+			entries.forEach(entry => {
+				console.log(entry); 
+				// if(entry.isIntersecting) {
+				// 	console.log('hit')
+				// }
+			})
+			// !entry.isIntersecting ? header.classList.add(...list) : header.classList.remove(...list)
+	}
 
-const navbarBtn = document.querySelector(".navbar-toggler");
-navbarBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	navbarBtn.classList.toggle("open-menu");
-});
+	const observer = new IntersectionObserver(callBack, option);
 
-const swiper = new Swiper("#articles .swiper", {
-	slidesPerView: 1,
-	spaceBetween: 24,
-	breakpoints: {
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 24,
+	sections.forEach(section => {
+		console.log(section)
+		observer.observe(section)
+	})
+
+
+	const navbarBtn = document.querySelector(".navbar-toggler");
+	navbarBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		navbarBtn.classList.toggle("open-menu");
+	});
+
+	const swiper = new Swiper("#articles .swiper", {
+		slidesPerView: 1,
+		spaceBetween: 24,
+		breakpoints: {
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 24,
+			},
+			// when window width is >= 640px
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 24,
+			},
 		},
-		// when window width is >= 640px
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 24,
+		pagination: {
+			el: "#articles .swiper-pagination",
+			clickable: true,
+			renderBullet: function (index, className) {
+				return `<a href="javascript:;" class="${className} fs-0 w-3 h-3 mx-2 my-2 bg-black">${index + 1}</a>`;
+			},
 		},
-	},
-	pagination: {
-		el: "#articles .swiper-pagination",
-		clickable: true,
-		renderBullet: function (index, className) {
-			return `<a href="javascript:;" class="${className} fs-0 w-3 h-3 mx-2 my-2 bg-black">${index + 1}</a>`;
-		},
-	},
-});
+	});
+
+})
